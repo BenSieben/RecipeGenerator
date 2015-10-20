f97ae2cdfcb24f34891694c576c248a48af79f6c
Ben Sieben <waripod@hotmail.com>
2015-10-20 11:36:31 -0700

    Added more comments to further document and explain the code within RecipeTextArea.java

--- src/RecipeTextArea.java
+++ src/RecipeTextArea.java
@@ -17,22 +17,27 @@ public class RecipeTextArea extends JPanel{
     //Holds the JLabel used to define this specific RecipeTextArea
     private JLabel label;
 
+    /**
+     * Creates a new RecipeTextArea with specific values
+     * @param labelText the text that will be put as a JLabel for the RecipeTextArea
+     * @param initialText the text that will initially be inside the JTextArea of the RecipeTextArea
+     */
     public RecipeTextArea(String labelText, String initialText) {
         textArea = new JTextArea(initialText, 5, 20);
         textArea.setEditable(true);
         textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
-        textArea.setBackground(new Color(0, 52, 52));
-        textArea.setForeground(new Color(255, 255, 255));
-        textArea.setCaretColor(new Color(255, 255, 255));
+        textArea.setWrapStyleWord(true); //Make line wrap go by words instead of by characters (the default)
+        textArea.setBackground(new Color(0, 52, 52));  //Set background color for the textArea
+        textArea.setForeground(new Color(255, 255, 255)); //Set font color for the textArea
+        textArea.setCaretColor(new Color(255, 255, 255)); //Set caret (i.e., text editing location indicator) color for the textArea
 
         scrollPane = new JScrollPane(textArea);
 
         label = new JLabel();
         label.setOpaque(true); //Set the JLabel to be opaque since it is disabled by default
         label.setText(labelText);
-        label.setBackground(new Color(0, 52, 52));
-        label.setForeground(new Color(255, 255, 255));
+        label.setBackground(new Color(0, 52, 52)); //Set background color for the label
+        label.setForeground(new Color(255, 255, 255)); //Set font color for the label
 
 
         setLayout(new BorderLayout());
