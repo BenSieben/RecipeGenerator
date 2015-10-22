073e197e8bdbc99da3f7701d858d0e475385feff
Ben Sieben <waripod@hotmail.com>
2015-10-21 17:31:40 -0700

    Added the RecipeComboBox to list recipe categories for the user to pick from.

---
+++ src/RecipeComboBox.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * A modified JPanel with a JComboBox for usage in the program.
+ * It allows for users to pick a category of their
+ * recipes from a set of defaults.
+ */
+public class RecipeComboBox extends JPanel {
+
+    private JLabel label;
+    private JComboBox<String> comboBox;
+    private String[] options;
+
+    public RecipeComboBox(String labelText, ArrayList<String> options) {
+        label = new JLabel();
+        label.setText(labelText);
+
+        this.options = options.toArray(new String[options.size()]);
+
+        comboBox = new JComboBox<>(this.options);
+        comboBox.setEditable(false);
+
+        setLayout(new BorderLayout());
+
+        add(label, BorderLayout.WEST);
+        add(comboBox, BorderLayout.CENTER);
+    }
+
+    public String toString() {
+        String output = (label.getText() + "\n\r");
+        for(String choice: options) {
+            output += (choice + "\n\r");
+        }
+        return output;
+    }
+}
--- src/RecipePanel.java
+++ src/RecipePanel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * JPanel that holds all of the components of the recipe generator needed for user usage
@@ -14,30 +15,78 @@ public class RecipePanel extends JPanel {
 
         RecipeTextField recipeTextField = new RecipeTextField("Recipe","Please write the recipe name here.");
         c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        c.gridwidth = 2;
-        c.weightx = 1;
+        c.gridx = 0; //Put this Component at the first column
+        c.gridy = 0; //Put this Component at the first row
+        c.gridwidth = 2; //This Component is two cells wide
+        c.weightx = 1; //Make the recipeTextField span the entire horizontal space of the JPanel
         add(recipeTextField, c);
         System.out.println(recipeTextField);
 
+        ArrayList<String> categoryOptions = getCategoryOptions();
+        RecipeComboBox categoryComboBox = new RecipeComboBox("Category",categoryOptions);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0; //Put this Component at the first column
+        c.gridy = 1; //Put this Component at the second row
+        c.gridwidth = 2; //This Component is two cells wide
+        c.weightx = 1; //Make the recipeTextField span the entire horizontal space of the JPanel
+        add(categoryComboBox, c);
+        System.out.println(recipeTextField);
+
+
         RecipeTextArea ingredientsTextArea = new RecipeTextArea("Ingredients","Please list ingredients here, separating each ingredient with an enter space.");
         c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 1;
-        c.gridwidth = 1;
-        c.weightx = 0.5;
+        c.gridx = 0; //Put this Component at the first column
+        c.gridy = 2; //Put this Component at the third row
+        c.gridwidth = 1; //This Component is one cell wide
+        c.weightx = 0.5; //Make the ingredientsTextArea span half the horizontal space of the JPanel
         add(ingredientsTextArea, c);
         System.out.println(ingredientsTextArea);
 
         RecipeTextArea instructionsTextArea = new RecipeTextArea("Instructions","Please list instructions here, separating each instruction with an enter space.");
         c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 1;
-        c.gridy = 1;
-        c.gridwidth = 1;
-        c.weightx = 0.5;
+        c.gridx = 1; //Put this Component at the second column
+        c.gridy = 2; //Put this Component at the third row
+        c.gridwidth = 1; // This Component is one cell wide
+        c.weightx = 0.5; //Make the instructionsTextArea span half the horizontal space of the JPanel
         add(instructionsTextArea, c);
         System.out.println(instructionsTextArea);
+    }
+
+
+    /**
+     * Returns a list of all the different categories
+     * that recipes are usually attributed to, with a
+     * "Other" option for anything else
+     * @return an ArrayList of all recipe categories
+     */
+    private ArrayList<String> getCategoryOptions() {
+        ArrayList<String> categoryOptions = new ArrayList<>();
+        categoryOptions.add("Appetizer / Starter");
+        categoryOptions.add("Bread / Roll / Muffin");
+        categoryOptions.add("Bean / Grain / Legume");
+        categoryOptions.add("Burger");
+        categoryOptions.add("Cake / Cupcake");
+        categoryOptions.add("Candy / Sweet");
+        categoryOptions.add("Casserole / Gratin");
+        categoryOptions.add("Cocktail");
+        categoryOptions.add("Cookie");
+        categoryOptions.add("Dessert");
+        categoryOptions.add("Dip / Spread");
+        categoryOptions.add("Dressing");
+        categoryOptions.add("Marinade / Rub");
+        categoryOptions.add("Nonalcoholic Drink");
+        categoryOptions.add("Pasta / Noodles");
+        categoryOptions.add("Pie / Tart");
+        categoryOptions.add("Pizza");
+        categoryOptions.add("Pudding / Custard");
+        categoryOptions.add("Salad");
+        categoryOptions.add("Sandwich");
+        categoryOptions.add("Sauce / Condiment");
+        categoryOptions.add("Side Dish");
+        categoryOptions.add("Soup / Stew");
+        categoryOptions.add("Stuffing");
+        categoryOptions.add("Other");
+        return categoryOptions;
     }
 
 }
